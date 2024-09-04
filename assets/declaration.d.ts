41c91fa87c0cb4a04e111803dab49fa2a7513b83
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-09-04 14:48:31 +0700

    declare module

---
+++ assets/declaration.d.ts
@@ -0,0 +1,29 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.raw" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.mp3" {
+  const value: string;
+  export default value;
+}
