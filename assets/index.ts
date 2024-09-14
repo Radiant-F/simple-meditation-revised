6c99c91da9b76c6ebe3829eedfcc6549a624ea59
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-09-14 19:27:47 +0700

    initial commit

---
+++ assets/index.ts
@@ -0,0 +1,75 @@
+// Meditation image assets
+import ImgMedBeach from "./images/meditation/beach.webp";
+import ImgMedUnderTree from "./images/meditation/meditate-under-tree.webp";
+import ImgMedRiver from "./images/meditation/river.webp";
+import ImgMedTrees from "./images/meditation/trees.webp";
+import ImgMedWaterfall from "./images/meditation/waterfall.webp";
+import ImgMedStars from "./images/meditation/yosemite-stars.webp";
+export {
+  ImgMedBeach,
+  ImgMedUnderTree,
+  ImgMedRiver,
+  ImgMedTrees,
+  ImgMedWaterfall,
+  ImgMedStars,
+};
+
+// Affirmation image assets
+import ImgAffCali1 from "./images/affirmation/California-backyard-1.webp";
+import ImgAffCali2 from "./images/affirmation/California-backyard-2.webp";
+import ImgAffCali3 from "./images/affirmation/California-backyard-3.webp";
+import ImgAffCali4 from "./images/affirmation/California-backyard-4.webp";
+import ImgAffEnCS1 from "./images/affirmation/english-countryside-1.webp";
+import ImgAffEnCS2 from "./images/affirmation/english-countryside-2.webp";
+import ImgAffEnCS3 from "./images/affirmation/english-countryside-3.webp";
+import ImgAffEnCS4 from "./images/affirmation/english-countryside-4.webp";
+import ImgAffMountainM1 from "./images/affirmation/mountain-meditate-1.webp";
+import ImgAffMountainM2 from "./images/affirmation/mountain-meditate-2.webp";
+import ImgAffMountainM3 from "./images/affirmation/mountain-meditate-3.webp";
+import ImgAffMountainM4 from "./images/affirmation/mountain-meditate-4.webp";
+import ImgAffNightSky1 from "./images/affirmation/night-sky-1.webp";
+import ImgAffNightSky2 from "./images/affirmation/night-sky-2.webp";
+import ImgAffNightSky3 from "./images/affirmation/night-sky-3.webp";
+import ImgAffNightSky4 from "./images/affirmation/night-sky-4.webp";
+import ImgAffOregon1 from "./images/affirmation/oregon-1.webp";
+import ImgAffOregon2 from "./images/affirmation/oregon-2.webp";
+import ImgAffOregon3 from "./images/affirmation/oregon-3.webp";
+import ImgAffOregon4 from "./images/affirmation/oregon-4.webp";
+import ImgAffRelaxingR1 from "./images/affirmation/relaxing-river-1.webp";
+import ImgAffRelaxingR2 from "./images/affirmation/relaxing-river-2.webp";
+import ImgAffRelaxingR3 from "./images/affirmation/relaxing-river-3.webp";
+import ImgAffRelaxingR4 from "./images/affirmation/relaxing-river-4.webp";
+import ImgAffTuscanny1 from "./images/affirmation/Tuscanny-1.webp";
+import ImgAffTuscanny2 from "./images/affirmation/Tuscanny-2.webp";
+import ImgAffTuscanny3 from "./images/affirmation/Tuscanny-3.webp";
+import ImgAffTuscanny4 from "./images/affirmation/Tuscanny-4.webp";
+export {
+  ImgAffCali1,
+  ImgAffCali2,
+  ImgAffCali3,
+  ImgAffCali4,
+  ImgAffEnCS1,
+  ImgAffEnCS2,
+  ImgAffEnCS3,
+  ImgAffEnCS4,
+  ImgAffMountainM1,
+  ImgAffMountainM2,
+  ImgAffMountainM3,
+  ImgAffMountainM4,
+  ImgAffNightSky1,
+  ImgAffNightSky2,
+  ImgAffNightSky3,
+  ImgAffNightSky4,
+  ImgAffOregon1,
+  ImgAffOregon2,
+  ImgAffOregon3,
+  ImgAffOregon4,
+  ImgAffRelaxingR1,
+  ImgAffRelaxingR2,
+  ImgAffRelaxingR3,
+  ImgAffRelaxingR4,
+  ImgAffTuscanny1,
+  ImgAffTuscanny2,
+  ImgAffTuscanny3,
+  ImgAffTuscanny4,
+};
