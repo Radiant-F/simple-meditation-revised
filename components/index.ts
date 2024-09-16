e240a123e3220180dbbc2fb2b4f564820d5c2e8d
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-09-16 09:47:17 +0700

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import Background from "./Background";
+import Gap from "./Gap";
+import ButtonPrimary from "./ButtonPrimary";
+import AbsoluteGradient from "./AbsoluteGradient";
+import Header from "./Header";
+
+export { Gap, Background, ButtonPrimary, AbsoluteGradient, Header };
